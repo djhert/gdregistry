e9bc2eb6e077a912f3d09158507028a5b6084fe1
davidh <david.hertenstein@gmail.com>
2020-05-22 22:25:37 -0400

    fixed regression of headers

--- gdnativedef.hpp
+++ gdnativedef.hpp
@@ -1,44 +1,78 @@
 #ifndef _GDNATIVEDEF_H_
 #define _GDNATIVEDEF_H_
 
+#if __has_include(<version.hpp>)
+#define _VERSIONED
+#include <version.hpp>
+#endif
+
 namespace godot {
 
 #define GDCLASS(T, G)                \
 	GODOT_CLASS(T, G)                \
+private:                             \
+	static bool _reg;                \
+                                     \
 public:                              \
 	T(){};                           \
 	~T();                            \
 	static void _register_methods(); \
-	void _init();                    \
-                                     \
-private:                             \
-	static bool _reg;
+	void _init();
 
 #define GDCLASS_PARENT(T, G)         \
 	GODOT_CLASS(T, G)                \
+private:                             \
+	static bool _reg;                \
+                                     \
 public:                              \
 	T(){};                           \
 	~T();                            \
 	static void _register_methods(); \
-	void _init();                    \
-	virtual void Init();             \
-                                     \
-private:                             \
-	static bool _reg;
+	virtual void _init();
 
-#define GDCLASS_CHILD(T, U)          \
-	GODOT_CLASS(T, U)                \
-public:                              \
-	T(){};                           \
-	~T();                            \
-	static void _register_methods(); \
-	void _init() { U::_init(); };    \
-                                     \
-private:                             \
-	static bool _reg;
+#define GDCLASS_CHILD(T, U) \
+	GODOT_SUBCLASS(T, U)    \
+private:                    \
+	static bool _reg;       \
+                            \
+public:                     \
+	T(){};                  \
+	~T();                   \
+	static void _register_methods();
 
-#define GDREGISTER(T) \
+// Macros for easy additons to GDRegistry
+#define _GDREG_1(T) \
 	bool T::_reg = godot::GDRegistry::Register<T>();
+
+#define _GDREG_2(T, P) \
+	bool T::_reg = godot::GDRegistry::Register<T>(P);
+
+#define _GDREGMACRO(_1, _2, _GDMAC, ...) _GDMAC
+
+// GDREGISTER is a macro for easy addition to GDRegistry
+// Can accept 1 or 2 parameters
+// GDREGISTER(TYPE) -- Sets priority 0
+// GDREGISTER(TYPE, PRIORITY) -- Sets priority to value
+#define GDREGISTER(...)                          \
+	_GDREGMACRO(__VA_ARGS__, _GDREG_2, _GDREG_1) \
+	(__VA_ARGS__)
+
+////////// Tool Registry
+#define _GDREGT_1(T) \
+	bool T::_reg = godot::GDRegistry::RegisterTool<T>();
+
+#define _GDREGT_2(T, P) \
+	bool T::_reg = godot::GDRegistry::RegisterTool<T>(P);
+
+#define _GDREGTMACRO(_1, _2, _GDTMAC, ...) _GDTMAC
+
+// GDREGISTERTOOL is a macro for easy addition to GDRegistry
+// Can accept 1 or 2 parameters
+// GDREGISTERTOOL(TYPE) -- Sets priority 10
+// GDREGISTERTOOL(TYPE, PRIORITY) -- Sets priority to value
+#define GDREGISTERTOOL(...)                         \
+	_GDREGTMACRO(__VA_ARGS__, _GDREGT_2, _GDREGT_1) \
+	(__VA_ARGS__)
 
 } // namespace godot
 #endif
--- gdregistry.hpp
+++ gdregistry.hpp
@@ -2,18 +2,45 @@
 #define _GDREGISTRY_HPP_
 
 #include <Godot.hpp>
+#include <algorithm>
+#include <memory>
 #include <vector>
+
+#include "gdnativedef.hpp"
 
 namespace godot {
 
 class GDRegistry {
+
+private:
+	// reg is the storage class to hold the functions
+	struct reg {
+		void (*f)();
+		int priority;
+	};
+	// _regSort is used as the sorting function for the Vector
+	static bool _regSort(const reg &a, const reg &b);
+
 public:
-	// Register is a Template Function that will place the object in the Registry
-	// It stores a pointer to the godot::register_class function to initialize
-	// the class.  Should be used in a static context in the class that uses it
+	// Register will place the object in the Registry
+	// It stores a pointer to the godot::register_class function to initialize the class
+	// Should be used in a static context in the class that uses it
+	// Optional priority parameter specifies order of registration, highest to lowest
+	// Default priority of 0
 	template <typename T>
-	static bool Register() {
-		GDRegistry::instance()->_gens.push_back(godot::register_class<T>);
+	static bool Register(const int &priority = 0) {
+		GDRegistry::instance()->_gens.push_back({ godot::register_class<T>, priority });
+		return true;
+	};
+
+	// RegisterTool will place the object in the Registry
+	// It stores a pointer to the godot::register_tool_class function to initialize the class.
+	// Should be used in a static context in the class that uses it
+	// Optional priority parameter specifies order of registration, highest to lowest
+	// Default priority of 10
+	template <typename T>
+	static bool RegisterTool(const int &priority = 10) {
+		GDRegistry::instance()->_gens.push_back({ godot::register_tool_class<T>, priority });
 		return true;
 	};
 
@@ -23,15 +50,15 @@ public:
 private:
 	// Follow the Singleton pattern
 	GDRegistry(){};
-	GDRegistry(const GDNRegistry &){};
+	GDRegistry(const GDRegistry &){};
 	~GDRegistry();
 
 	// static instances for the Singleton
 	static GDRegistry *instance();
 	static GDRegistry *_instance;
 
-	// std::vector of void functions
-	std::vector<void (*)()> _gens;
+	// std::vector of reg struct
+	std::vector<reg> _gens;
 };
 
 } // namespace godot
